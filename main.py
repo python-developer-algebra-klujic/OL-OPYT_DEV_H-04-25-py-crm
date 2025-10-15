81266de9e2e7146dd1d7a9701eb5cc0a6e3f0e86
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-10-15 20:44:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ from typing import Dict
 
 
 class Repository:
-    def __init__(self):
-        pass
+    def __init__(self, file_path: str):
+        # private property
+        self._file_path = file_path
 
     def save(self, data):
         print('save() is working')
 
     def get_all(self):
         try:
-            with open('./data_store/customers.json', 'r', encoding='utf-8') as file_reader:
+            with open(self._file_path, 'r', encoding='utf-8') as file_reader:
                 return json.load(file_reader)
         except Exception as ex:
             print(f'{dt.now()} - Dogodila se greska {ex}!')
