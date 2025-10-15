486c0f9d87c632aacc304e8fb1c92351250ec3c1
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-10-15 20:38:34 +0200

    building app init

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 from datetime import datetime as dt
+import sys
+from typing import Dict
 
 
 class Repository:
@@ -27,9 +29,33 @@ class Repository:
         print('delete() is working')
 
 
-def main():
-    repo = Repository()
+# Funkcija koja inicijalizira pocetne postavke nase aplikacije
+def app_init() -> Dict:
+    try:
+        with open('./data_store/settings.json') as file_reader:
+            # ako sve dobro prode onda ce vratiti rjecnik s postavkama
+            return json.load(file_reader)
+    except Exception as ex:
+        # ako se dogodi greska onda ce vratiti rjecnik s error porukom
+        return {
+            'error_message': f'{dt.now()} - {ex}'
+        }
 
+
+# Glavna funkcija iz koje se pokrece aplikacija
+def main():
+    # ucitamo postavke aplikacije u rjecnik
+    app_config = app_init()
+
+    # provjerimo je li bilo gresaka, odnosno ima li podataka pod kljucem error_message
+    if app_config['error_message'] != '':
+        print(app_config['error_message'])
+        # sys.exit()
+        return
+    else:
+        repo = Repository(app_config['file_path'])
+
+    # CRUD (Create, Read ili Retreive, Update, Delete) metode repozitorija
     repo.save([])
     repo.get_all()
     repo.get(5)
