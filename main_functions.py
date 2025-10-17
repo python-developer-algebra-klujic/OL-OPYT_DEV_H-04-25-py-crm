feb465f10b192a8dbbf03370f90ce19fcaaa5327
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-10-17 18:19:09 +0200

    Update main_functions.py

--- main_functions.py
+++ main_functions.py
@@ -1,8 +1,10 @@
 import json
 from datetime import datetime as dt
-import sys
 from typing import Dict, List
 
+
+customers = []
+file_path = ''
 
 class Customer:
     def __init__(self, id, name, vat_id, email, phone):
@@ -16,53 +18,48 @@ class Customer:
         return f'({self.id})\t{self.name}'
 
 
-class Repository:
-    def __init__(self, file_path: str):
-        # private property - sugerira da je samo za primjenu unutar klase
-        self._file_path = file_path
-        # kod kreiranja objekta ucitaju se svi podaci i napuni se ovo svojstvo
-        self._customers = self._load_data()
 
-    def save(self, data):
-        print('save() is working')
+def save_customer(data):
+    print('save() is working')
 
-    def get_all(self) -> List[Customer]:
-        return self._customers
+def get_all_customers() -> List[Customer]:
+    return customers
 
-    def get(self, id: int) -> Customer:
-        return self._customers[id - 1]
+def get_customer(id: int) -> Customer:
+    return customers[id - 1]
 
-    def update(self, data):
-        print('update() is working')
+def update_customer(data):
+    print('update() is working')
 
-    def delete(self, id):
-        print('delete() is working')
+def delete_customer(id):
+    print('delete() is working')
 
-    # privatna metoda koja rjecnik pretvori u objekt klase Customer
-    def _dict_to_customer(self, dictionary):
-        return Customer(dictionary['id'],
-                        dictionary['name'],
-                        dictionary['vat_id'],
-                        dictionary['email'],
-                        dictionary['phone'])
+# privatna metoda koja rjecnik pretvori u objekt klase Customer
+def dict_to_customer(dictionary):
+    return Customer(dictionary['id'],
+                    dictionary['name'],
+                    dictionary['vat_id'],
+                    dictionary['email'],
+                    dictionary['phone'])
 
-    # privatna metoda koja objekt klase Customer pretvori u rjecnik
-    def _customer_to_dict(self):
-        pass
+# privatna metoda koja objekt klase Customer pretvori u rjecnik
+def customer_to_dict():
+    pass
 
-    def _load_data(self):
-        try:
-            with open(self._file_path, 'r', encoding='utf-8') as file_reader:
-                customers = []
-                # ucitamo listu rjecnika i konvertiramo ih u listu objekata klase Customer
-                data = json.load(file_reader)
-                for element in data:
-                    customer = self._dict_to_customer(element)
-                    customers.append(customer)
-                return customers
-        except Exception as ex:
-            print(f'{dt.now()} - Dogodila se greska {ex}!')
-            return []
+def load_data(file_path: str):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file_reader:
+            # Ocisti listu od starih podataka prije ucitavanja
+            customers.clear()
+            # ucitamo listu rjecnika i konvertiramo ih u listu objekata klase Customer
+            data = json.load(file_reader)
+            for element in data:
+                customer = dict_to_customer(element)
+                customers.append(customer)
+            return customers
+    except Exception as ex:
+        print(f'{dt.now()} - Dogodila se greska {ex}!')
+        return []
 
 
 # Funkcija koja inicijalizira pocetne postavke nase aplikacije
@@ -80,24 +77,27 @@ def app_init() -> Dict:
 
 # Glavna funkcija iz koje se pokrece aplikacija
 def main():
+    # Ovo moram napraviti jer mijenjam
+    # varijablu unutar funkcije
+    global customers
     # ucitamo postavke aplikacije u rjecnik
     app_config = app_init()
+    # Prije nastavka ucitam sve podatke u listu
+    customers = load_data(app_config['file_path'])
 
     # provjerimo je li bilo gresaka, odnosno ima li podataka pod kljucem error_message
     if app_config['error_message'] != '':
         print(app_config['error_message'])
         # sys.exit()
         return
-    else:
-        repo = Repository(app_config['file_path'])
 
     # CRUD (Create, Read ili Retreive, Update, Delete) metode repozitorija
     # repo.save([])
-    all_customers = repo.get_all()
+    all_customers = get_all_customers()
     for customer in all_customers:
         print(customer)
     print()
-    customer_5 = repo.get(25)
+    customer_5 = get_customer(25)
     print(customer_5.phone)
     # repo.update([])
     # repo.delete(13)
